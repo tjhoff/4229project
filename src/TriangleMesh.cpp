53c9fc87a00ba2bb45f4838426923ffe6a991a09
TJ Hoff <king.of.lepers@gmail.com>
2011-10-23 18:36:04 -0600

    triangle textures working, looks ugly as hell

--- src/TriangleMesh.cpp
+++ src/TriangleMesh.cpp
@@ -4,9 +4,8 @@
 #define vertex(i,j) glVertex3f(tVertices[i][j].x,tVertices[i][j].y,tVertices[i][j].z)
 #define color(i,j) glColor3f(1.0, 1.0, 1.0)
 #define normal(i,j) glNormal3f(tNormals[i][j].x,tNormals[i][j].y,tNormals[i][j].z)
-#define tex(i,j) glTexCoord3f((double)i/width, heightmap[i][j], (double)j/height); qDebug() << i << width << (double)i/width << "...";
 
-#define ColNormVex(i,j)   normal(i,j); color(i,j); tex(i,j); vertex(i,j)
+#define ColNormVex(i,j)   normal(i,j); color(i,j); vertex(i,j)
 
 TriangleMesh::TriangleMesh(Vec3 ** vertices, Vec3 ** colors, int vwidth, int vheight){
 
@@ -19,16 +18,16 @@ TriangleMesh::TriangleMesh(Vec3 ** vertices, Vec3 ** colors, int vwidth, int vhe
 	
 	heightmap = new float*[height];
 
-	for(int i = 0; i < height; i++){
-		tVertices[i] = new Vec3[width];
-		tColors[i] = new Vec3[width];
-		tNormals[i] = new Vec3[width];
-		heightmap[i] = new float[width];
+	for(int iz = 0; iz < height; iz++){
+		tVertices[iz] = new Vec3[width];
+		tColors[iz] = new Vec3[width];
+		tNormals[iz] = new Vec3[width];
+		heightmap[iz] = new float[width];
 		
-		for(int j = 0; j<width; j++){
-			tVertices[i][j] = vertices[i][j];
-			tColors[i][j] = colors[i][j];
-			heightmap[i][j] = vertices[i][j].y;
+		for(int ix = 0; ix<width; ix++){
+			tVertices[iz][ix] = vertices[iz][ix];
+			tColors[iz][ix] = colors[iz][ix];
+			heightmap[iz][ix] = vertices[iz][ix].y;
 		}
 	}
 	double start_time = clock()/1000;
@@ -51,36 +50,78 @@ void TriangleMesh::compile(){
 	glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
 	
 	glColor3f(1.0, 1.0, 1.0);
-	
-	glBegin(GL_QUADS);
-	for(int i = 0; i<height-1; i++)
-	{ 
-		for(int j = 0; j<width-1; j++)
-		{ 
-			glNormal3f(tNormals[i][j].x,tNormals[i][j].y,tNormals[i][j].z);
-			glTexCoord3f((double)i/height, (double)j/width, heightmap[i][j]);
-			glVertex3f(tVertices[i][j].x,tVertices[i][j].y,tVertices[i][j].z);
-			
-			glNormal3f(tNormals[i+1][j].x,tNormals[i+1][j].y,tNormals[i+1][j].z);
-			glTexCoord3f((double)(i+1)/height, (double)j/width, heightmap[i+1][j+1]);
-			glVertex3f(tVertices[i+1][j].x,tVertices[i+1][j].y,tVertices[i+1][j].z);
-			
-			glNormal3f(tNormals[i+1][j+1].x,tNormals[i+1][j+1].y,tNormals[i+1][j+1].z);
-			glTexCoord3f((double)(i+1)/height, (double)(j+1)/width, heightmap[i+1][j+1]);
-			glVertex3f(tVertices[i+1][j+1].x,tVertices[i+1][j+1].y,tVertices[i+1][j+1].z);
-			
-			glNormal3f(tNormals[i][j+1].x,tNormals[i][j+1].y,tNormals[i][j+1].z);
-			glTexCoord3f((double)i/height, (double)(j+1)/width, heightmap[i][j+1]);
-			glVertex3f(tVertices[i][j+1].x,tVertices[i][j+1].y,tVertices[i][j+1].z);
-			
-		} 
-	}
-	
-	
-	glEnd();
-	
-	qDebug()<<"Compiling displays took "<< clock()/1000 - start_time<< "parsecs";
 
+	for(int iz = 0; iz<height-1; iz++){ 
+		glBegin(GL_TRIANGLES);
+		for(int ix = 0; ix<width-2; ix+=2){ 
+			switch (count){
+				case 0:
+					glTexCoord2f((double)(iz)/height, (double)(ix)/width);
+					ColNormVex(iz,ix);
+					glTexCoord2f((double)(iz+1)/height, (double)(ix)/width);
+					ColNormVex(iz+1,ix);
+					glTexCoord2f((double)(iz)/height, (double)(ix+1)/width);
+					ColNormVex(iz,ix+1);
+
+					glTexCoord2f((double)(iz)/height, (double)(ix+1)/width);
+					ColNormVex(iz,ix+1);
+					glTexCoord2f((double)(iz+1)/height, (double)(ix)/width);
+					ColNormVex(iz+1,ix);
+					glTexCoord2f((double)(iz+1)/height, (double)(ix+1)/width);
+					ColNormVex(iz+1,ix+1);
+					
+					glTexCoord2f((double)(iz)/height, (double)(ix+1)/width);
+					ColNormVex(iz,ix+1);
+					glTexCoord2f((double)(iz+1)/height, (double)(ix+1)/width);
+					ColNormVex(iz+1,ix+1);
+					glTexCoord2f((double)(iz+1)/height, (double)(ix+2)/width);
+					ColNormVex(iz+1,ix+2);
+					
+					glTexCoord2f((double)(iz)/height, (double)(ix+1)/width);
+					ColNormVex(iz,ix+1);
+					glTexCoord2f((double)(iz+1)/height, (double)(ix+2)/width);
+					ColNormVex(iz+1,ix+2);
+					glTexCoord2f((double)(iz)/height, (double)(ix+2)/width);
+					ColNormVex(iz,ix+2);
+					break;
+				case 1:
+					glTexCoord2f((double)(iz)/height, (double)(ix)/width);
+					ColNormVex(iz,ix);
+					glTexCoord2f((double)(iz+1)/height, (double)(ix)/width);
+					ColNormVex(iz+1,ix);
+					glTexCoord2f((double)(iz+1)/height, (double)(ix+1)/width);
+					ColNormVex(iz+1,ix+1);
+					
+					glTexCoord2f((double)(iz)/height, (double)(ix)/width);
+					ColNormVex(iz,ix);
+					glTexCoord2f((double)(iz+1)/height, (double)(ix+1)/width);
+					ColNormVex(iz+1,ix+1);
+					glTexCoord2f((double)(iz)/height, (double)(ix+1)/width);
+					ColNormVex(iz,ix+1);
+
+					glTexCoord2f((double)(iz)/height, (double)(ix+1)/width);
+					ColNormVex(iz,ix+1);
+					glTexCoord2f((double)(iz+1)/height, (double)(ix+1)/width);
+					ColNormVex(iz+1,ix+1);
+					glTexCoord2f((double)(iz)/height, (double)(ix+2)/width);
+					ColNormVex(iz,ix+2);
+					
+					glTexCoord2f((double)(iz+1)/height, (double)(ix+1)/width);
+					ColNormVex(iz+1,ix+1);
+					glTexCoord2f((double)(iz+1)/height, (double)(ix+2)/width);
+					ColNormVex(iz+1,ix+2);
+					glTexCoord2f((double)(iz)/height, (double)(ix+2)/width);
+					ColNormVex(iz,ix+2);
+					break;
+			}
+
+
+		
+		} // end ix
+		count = !count;
+		glEnd();
+	} // end iz
+	
 }
 
 void TriangleMesh::computeNormals(){
@@ -94,7 +135,7 @@ void TriangleMesh::computeNormals(){
 	
 	float x1,y1, z1;
 	
-	/////////////// DO FOR i=0 j=0	
+	/////////////// DO FOR iz=0 ix=0	
 	v1 = tVertices[0][0]; //
 	v2 = tVertices[1][0]; // 
 	v3 = tVertices[1][1]; //
@@ -108,12 +149,12 @@ void TriangleMesh::computeNormals(){
 	////////////////////////////////
 	
 
-	for(int j = 1;j<width-1;j++){
+	for(int ix = 1;ix<width-1;ix++){
 	
-		//////////// FOR i=0
-		v1 = tVertices[0][j]; //
-		v2 = tVertices[0][j-1]; // 
-		v3 = tVertices[1][j]; //
+		//////////// FOR iz=0
+		v1 = tVertices[0][ix]; //
+		v2 = tVertices[0][ix-1]; // 
+		v3 = tVertices[1][ix]; //
 			
 		u = v2 - v1;
 		v = v3 - v1;
@@ -122,20 +163,20 @@ void TriangleMesh::computeNormals(){
 		y1 = (u.z*v.x) - (u.x*v.z);
 		z1 = (u.x*v.y) - (u.y*v.x);
 				
-		v1 = tVertices[0][j]; //
-		v2 = tVertices[1][j]; // 
-		v3 = tVertices[0][j+1]; //
+		v1 = tVertices[0][ix]; //
+		v2 = tVertices[1][ix]; // 
+		v3 = tVertices[0][ix+1]; //
 			
 		u = v2 - v1;
 		v = v3 - v1;
 			
-		tNormals[0][j].x = (x1+(u.y*v.z) - (u.z*v.y))/2;
-		tNormals[0][j].y = (y1+(u.z*v.x) - (u.x*v.z))/2;
-		tNormals[0][j].z = (z1+(u.x*v.y) - (u.y*v.x))/2;
+		tNormals[0][ix].x = (x1+(u.y*v.z) - (u.z*v.y))/2;
+		tNormals[0][ix].y = (y1+(u.z*v.x) - (u.x*v.z))/2;
+		tNormals[0][ix].z = (z1+(u.x*v.y) - (u.y*v.x))/2;
 		////////////////////
 	}
 
-	////////////// i = 0, j = width-1		
+	////////////// iz = 0, ix = width-1		
 	v1 = tVertices[0][width-1]; //
 	v2 = tVertices[0][width-2]; // 
 	v3 = tVertices[1][width-1]; //
@@ -152,12 +193,12 @@ void TriangleMesh::computeNormals(){
 	tNormals[0][width-1].z = (z1+(u.x*v.y) - (u.y*v.x))/2;
 	//////////////////////////
 
-	for(int i = 1; i<height-1; i++){ 
+	for(int iz = 1; iz<height-1; iz++){ 
 
-		//////////// DO FOR j = 0
-		v1 = tVertices[i][0]; //
-		v2 = tVertices[i+1][0]; // 
-		v3 = tVertices[i][1]; //
+		//////////// DO FOR ix = 0
+		v1 = tVertices[iz][0]; //
+		v2 = tVertices[iz+1][0]; // 
+		v3 = tVertices[iz][1]; //
 			
 		u = v2 - v1;
 		v = v3 - v1;
@@ -166,26 +207,26 @@ void TriangleMesh::computeNormals(){
 		y1 = (u.z*v.x) - (u.x*v.z);
 		z1 = (u.x*v.y) - (u.y*v.x);
 				
-		v1 = tVertices[i][0]; //
-		v2 = tVertices[i][1]; // 
-		v3 = tVertices[i-1][0]; //
+		v1 = tVertices[iz][0]; //
+		v2 = tVertices[iz][1]; // 
+		v3 = tVertices[iz-1][0]; //
 			
 		u = v2 - v1;
 		v = v3 - v1;
 			
-		tNormals[i][0].x = (x1+(u.y*v.z) - (u.z*v.y))/2;
-		tNormals[i][0].y = (y1+(u.z*v.x) - (u.x*v.z))/2;
-		tNormals[i][0].z = (z1+(u.x*v.y) - (u.y*v.x))/2;
+		tNormals[iz][0].x = (x1+(u.y*v.z) - (u.z*v.y))/2;
+		tNormals[iz][0].y = (y1+(u.z*v.x) - (u.x*v.z))/2;
+		tNormals[iz][0].z = (z1+(u.x*v.y) - (u.y*v.x))/2;
 				
 		//////////////////////////
 
-		for (int j = 1; j<width-1; j++){
+		for (int ix = 1; ix<width-1; ix++){
 
 			///// BASE CASE //////
 			
-			v1 = tVertices[i-1][j]; //
-			v2 = tVertices[i][j-1]; //
-			v3 = tVertices[i+1][j]; //
+			v1 = tVertices[iz-1][ix]; //
+			v2 = tVertices[iz][ix-1]; //
+			v3 = tVertices[iz+1][ix]; //
 			
 			u = v2 - v1;
 			v = v3 - v1;
@@ -194,23 +235,23 @@ void TriangleMesh::computeNormals(){
 			y1 = (u.z*v.x) - (u.x*v.z);
 			z1 = (u.x*v.y) - (u.y*v.x);
 			
-			v1 = tVertices[i-1][j]; //
-			v2 = tVertices[i+1][j]; // 
-			v3 = tVertices[i][j+1]; // 
+			v1 = tVertices[iz-1][ix]; //
+			v2 = tVertices[iz+1][ix]; // 
+			v3 = tVertices[iz][ix+1]; // 
 			
 			u = v2 - v1;
 			v = v3 - v1;
 			
-			tNormals[i][j].x = (x1+(u.y*v.z) - (u.z*v.y))/2;
-			tNormals[i][j].y = (y1+(u.z*v.x) - (u.x*v.z))/2;
-			tNormals[i][j].z = (z1+(u.x*v.y) - (u.y*v.x))/2;
+			tNormals[iz][ix].x = (x1+(u.y*v.z) - (u.z*v.y))/2;
+			tNormals[iz][ix].y = (y1+(u.z*v.x) - (u.x*v.z))/2;
+			tNormals[iz][ix].z = (z1+(u.x*v.y) - (u.y*v.x))/2;
 			////////////////////
 			
 		}
-		////////////// j = width-1
-		v1 = tVertices[i][width-1]; //
-		v2 = tVertices[i-1][width-1]; // 
-		v3 = tVertices[i][width-2]; //
+		////////////// ix = width-1
+		v1 = tVertices[iz][width-1]; //
+		v2 = tVertices[iz-1][width-1]; // 
+		v3 = tVertices[iz][width-2]; //
 			
 		u = v2 - v1;
 		v = v3 - v1;
@@ -219,20 +260,20 @@ void TriangleMesh::computeNormals(){
 		y1 = (u.z*v.x) - (u.x*v.z);
 		z1 = (u.x*v.y) - (u.y*v.x);
 				
-		v1 = tVertices[i][width-1]; //
-		v2 = tVertices[i][width-2]; // 
-		v3 = tVertices[i+1][width-1]; //
+		v1 = tVertices[iz][width-1]; //
+		v2 = tVertices[iz][width-2]; // 
+		v3 = tVertices[iz+1][width-1]; //
 			
 		u = v2 - v1;
 		v = v3 - v1;
 			
-		tNormals[i][width-1].x = (x1+(u.y*v.z) - (u.z*v.y))/2;
-		tNormals[i][width-1].y = (y1+(u.z*v.x) - (u.x*v.z))/2;
-		tNormals[i][width-1].z = (z1+(u.x*v.y) - (u.y*v.x))/2;
+		tNormals[iz][width-1].x = (x1+(u.y*v.z) - (u.z*v.y))/2;
+		tNormals[iz][width-1].y = (y1+(u.z*v.x) - (u.x*v.z))/2;
+		tNormals[iz][width-1].z = (z1+(u.x*v.y) - (u.y*v.x))/2;
 				
 	}
 
-	///////////////// for i = height-1, j = 0;
+	///////////////// for iz = height-1, ix = 0;
 	v1 = tVertices[height-1][0]; //
 	v2 = tVertices[height-1][1]; // 
 	v3 = tVertices[height-2][0]; //
@@ -250,11 +291,11 @@ void TriangleMesh::computeNormals(){
 	//////////////////////////
 
 
-	for(int j =1; j<width-1;j++){
-		////////////////// for i = height-1
-		v1 = tVertices[height-1][j]; //
-		v2 = tVertices[height-2][j]; // 
-		v3 = tVertices[height-1][j-1]; //
+	for(int ix =1; ix<width-1;ix++){
+		////////////////// for iz = height-1
+		v1 = tVertices[height-1][ix]; //
+		v2 = tVertices[height-2][ix]; // 
+		v3 = tVertices[height-1][ix-1]; //
 			
 		u = v2 - v1;
 		v = v3 - v1;
@@ -263,19 +304,19 @@ void TriangleMesh::computeNormals(){
 		y1 = (u.z*v.x) - (u.x*v.z);
 		z1 = (u.x*v.y) - (u.y*v.x);
 				
-		v1 = tVertices[height-1][j]; //
-		v2 = tVertices[height-1][j+1]; // 
-		v3 = tVertices[height-2][j]; //
+		v1 = tVertices[height-1][ix]; //
+		v2 = tVertices[height-1][ix+1]; // 
+		v3 = tVertices[height-2][ix]; //
 			
 		u = v2 - v1;
 		v = v3 - v1;
 			
-		tNormals[height-1][j].x = (x1+(u.y*v.z) - (u.z*v.y))/2;
-		tNormals[height-1][j].y = (y1+(u.z*v.x) - (u.x*v.z))/2;
-		tNormals[height-1][j].z = (z1+(u.x*v.y) - (u.y*v.x))/2;
+		tNormals[height-1][ix].x = (x1+(u.y*v.z) - (u.z*v.y))/2;
+		tNormals[height-1][ix].y = (y1+(u.z*v.x) - (u.x*v.z))/2;
+		tNormals[height-1][ix].z = (z1+(u.x*v.y) - (u.y*v.x))/2;
 		////////////////////////
 	}
-	//////////////// for i = height-1, j = width -1
+	//////////////// for iz = height-1, ix = width -1
 	v1 = tVertices[height-1][width-1]; //
 	v2 = tVertices[height-2][width-1]; // 
 	v3 = tVertices[height-1][width-2]; //
